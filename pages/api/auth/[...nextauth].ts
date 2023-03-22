1e70134a7cde5c5e9eb053f9fac9da356f2fccb0
Nihat Emre Yeşil <96814623+nhtmrys@users.noreply.github.com>
2023-03-22 06:06:37 +0300

    google and github auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,20 +2,20 @@ import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import Credentials from 'next-auth/providers/credentials';
-/*import { PrismaAdapter } from '@next-auth/prisma-adapter';*/
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import {compare} from 'bcrypt';
 import prismadb from '@/lib/prismadb';
 
 export default NextAuth({
     providers: [
-        /* GithubProvider({
+         GithubProvider({
              clientId: process.env.GITHUB_ID || '',
              clientSecret: process.env.GITHUB_SECRET || '',
          }),
          GoogleProvider({
              clientId: process.env.GOOGLE_CLIENT_ID || '',
              clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-         }),*/
+         }),
         Credentials({
             id: 'credentials',
             name: 'Credentials',
@@ -58,6 +58,7 @@ export default NextAuth({
         signIn: '/auth'
     },
     debug: process.env.NODE_ENV === 'development',
+    adapter: PrismaAdapter(prismadb),
     session: {
         strategy: 'jwt',
 
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,6 +3,8 @@ import {useCallback, useState} from "react";
 import axios from "axios";
 import {signIn} from "next-auth/react";
 import {useRouter} from "next/router";
+import {FaGithub} from "react-icons/fa";
+import {FcGoogle} from "react-icons/fc";
 
 export default function Auth() {
 
@@ -32,7 +34,7 @@ export default function Auth() {
         } catch (err) {
             console.log(err)
         }
-    }, [email, password,router])
+    }, [email, password, router])
 
     const register = useCallback(async () => {
         try {
@@ -45,9 +47,7 @@ export default function Auth() {
         } catch (err) {
             console.log(err)
         }
-    }, [email, name, password,login])
-
-
+    }, [email, name, password, login])
 
 
     return (
@@ -95,6 +95,20 @@ export default function Auth() {
                                 className="bg-red-600 py-3 text-white rounded-md w-full hover:bg-red-700 transition">
                                 {variant === "login" ? "Login" : "Sign up"}
                             </button>
+                            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+
+                                <div
+                                    onClick={() => signIn("google", {callbackUrl: "/"})}
+                                    className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                    <FcGoogle size={30}/>
+                                </div>
+                                <div
+                                    onClick={() => signIn("github", {callbackUrl: "/"})}
+                                    className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                    <FaGithub size={30}/>
+                                </div>
+                            </div>
+
                             <p className="text-neutral-500 mt-12">
                                 {variant === "login" ? "First time using Netflix?" : "Already have an account?"}
                                 <span onClick={toggleVariant}
