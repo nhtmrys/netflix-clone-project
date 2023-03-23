db60e3cb7660d1bfb70674854e36001db88085f9
Nihat Emre Yeşil <96814623+nhtmrys@users.noreply.github.com>
2023-03-23 22:25:39 +0300

    billboard update

---
+++ hooks/useInfoModal.ts
@@ -0,0 +1,15 @@
+import create from "zustand";
+export interface ModalStoreInterface {
+  movieId?: string;
+  isOpen: boolean;
+  openModal: (movieId: string) => void;
+  closeModal: () => void;
+}
+const useInfoModal = create<ModalStoreInterface>((set) => ({
+  movieId: undefined,
+  isOpen: false,
+  openModal: (movieId: string) => set({ movieId, isOpen: true }),
+  closeModal: () => set({ movieId: undefined, isOpen: false }),
+}));
+
+export default useInfoModal;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,17 @@ import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
 import useMovieList from "@/hooks/useMovieList";
 import useFavorites from "@/hooks/useFavorites";
+import InfoModal from "@/components/InfoModal";
+import useInfoModal from "@/hooks/useInfoModal";
 
 export default function Home() {
   /* const { data: user = [] } = useCurrentUser();*/
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
